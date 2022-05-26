b3b8865547e3401dd018a3ac439059cb6d734e59
turenkovn <75043951+VladimirTurenko@users.noreply.github.com>
2022-05-26 08:49:18 +0300

    Update calculator_spec.js

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -6,27 +6,22 @@ describe('calculator', function () {
     calculator = new Calculator()
   });
 
-  // write unit tests here in the form of "it should do something..."
-  it('it has a sample test', function(){
-    assert.equal(true, true)
-  })
-
-  it('should be able to add', function() {
-    calculator.previousTotal = 4;
-    calculator.add(4);
-    assert.equal(calculator.runningTotal, 8);
+  it('Valid addition', function() {
+    calculator.previousTotal = 21;
+    calculator.add(22);
+    assert.equal(calculator.runningTotal, 43);
   })
   
-   it('Limit value check 0', function() {
+   it('Valid addition Two Zeros', function() {
     calculator.previousTotal = 0;
     calculator.add(0);
     assert.equal(calculator.runningTotal, 0);
   })
   
-  it('Limit value check left 0', function() {
+  it('Addition left argument 0', function() {
     calculator.previousTotal = 0;
-    calculator.add(5);
-    assert.equal(calculator.runningTotal, 5);
+    calculator.add(31);
+    assert.equal(calculator.runningTotal, 31);
   })
   
   it('Limit value check right 0', function() {
@@ -35,46 +30,64 @@ describe('calculator', function () {
     assert.equal(calculator.runningTotal, 6);
   })
   
-   it('Checking large limit values', function() {
-    calculator.previousTotal = 125;
-    calculator.add(125);
-    assert.equal(calculator.runningTotal, 250);
+   it('Checking the right boundary of additions', function() {
+    calculator.previousTotal = 125000;
+    calculator.add(125000);
+    assert.equal(calculator.runningTotal, 250000);
   })
 
   it('should be able to subtract', function() {
-    calculator.previousTotal = 4;
-    calculator.subtract(2);
-    assert.equal(calculator.runningTotal, 2);
+    calculator.previousTotal = 41;
+    calculator.subtract(20);
+    assert.equal(calculator.runningTotal, 21);
   })
   
   it('Subtraction check 0', function() {
-    calculator.previousTotal = 4;
+    calculator.previousTotal = 100;
     calculator.subtract(0);
-    assert.equal(calculator.runningTotal, 4);
+    assert.equal(calculator.runningTotal, 100);
+  })
+
+  it('subtraction of negative numbers', function() {
+    calculator.previousTotal = -100;
+    calculator.subtract(5);
+    assert.equal(calculator.runningTotal, -105);
+  })
+
+   it('subtraction two negative numbers', function() {
+    calculator.previousTotal = -100;
+    calculator.subtract(-5);
+    assert.equal(calculator.runningTotal, -95);
   })
 
   it('should be able to multiply', function() {
-    calculator.previousTotal = 2;
-    calculator.multiply(2);
-    assert.equal(calculator.runningTotal, 4);
+    calculator.previousTotal = 4;
+    calculator.multiply(8);
+    assert.equal(calculator.runningTotal, 32);
   })
   
    it('Checking multiplication by 0', function() {
-    calculator.previousTotal = 2;
+    calculator.previousTotal = 8;
     calculator.multiply(0);
     assert.equal(calculator.runningTotal, 0);
   })
 
+   it('Checking the Right Bound of Multiplication', function() {
+    calculator.previousTotal = 3896;
+    calculator.multiply(3896);
+    assert.equal(calculator.runningTotal, 15178816);
+  })
+
   it('Checking division by zero', function() {
-    calculator.previousTotal = 8;
-    calculator.divide(4);
-    assert.equal(calculator.runningTotal, 2);
+    calculator.previousTotal = 2354;
+    calculator.divide(0);
+    assert.equal(calculator.runningTotal, 0);
   })
   
    it('should be able to divide', function() {
-    calculator.previousTotal = 8;
-    calculator.divide(4);
-    assert.equal(calculator.runningTotal, 2);
+    calculator.previousTotal = 200;
+    calculator.divide(25);
+    assert.equal(calculator.runningTotal, 8);
   })
   
   it('Verification Zero is divided by the number', function() {
@@ -111,7 +124,7 @@ describe('calculator', function () {
     assert.equal(calculator.runningTotal, 0);
   })
   
-  it('should be able to clear total when clearClick is called =0', function() {
+  it('should be able to clear total when clearClick is called =0',function() {
     calculator.runningTotal = 0;
     calculator.clearClick();
     assert.equal(calculator.runningTotal, 0);
@@ -122,6 +135,8 @@ describe('calculator', function () {
   calculator.clearClick();
   assert.equal(calculator.previousOperator, null);
 })
-
+  it('it has a sample test', function(){
+    assert.equal(true, true)
+  })
 
 });
